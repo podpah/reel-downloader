1b3346c74b0cdbadaa9c1a791b223565bdefd81e
Podaph <107078260+podpah@users.noreply.github.com>
2023-08-09 20:53:45 +0100

    Upgrades, people. Upgrades.

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,11 +4,23 @@ javascript:(function() {
         var jsonData = JSON.parse(scriptTags[0].textContent);
         if (jsonData[0].video && jsonData[0].video[0].contentUrl) {
             var contentUrl = jsonData[0].video[0].contentUrl;
-            window.open(contentUrl, '_blank');
+            var openVideo = document.createElement('a');
+            openVideo.href = contentUrl;
+            openVideo.download = '';
+            document.body.appendChild(openVideo);
+            openVideo.click();
         } else {
             console.log("Video URL not found.");
         }
-    } else {
+    } else if (window.location.href.substring(8,16) == "scontent"){
+        var link = document.createElement('a');
+        link.href = window.location.href;
+        link.download = '';
+        document.body.appendChild(link);
+        link.click();
+        history.back();
+    } 
+    else {
         console.log("Script tag not found.");
     }
 })();
